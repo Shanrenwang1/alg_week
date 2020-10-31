fd112ff81fec670942842293fff8eb9b20f85952
fengl <im.fenglei@foxmail.com>
2020-10-31 10:41:20 +0800

    E edited

--- week_8/E.cpp
+++ week_8/E.cpp
@@ -16,6 +16,11 @@ using namespace std;
 int s[MAXSIZE][MAXSIZE];
 int2;
 int k, by, bx, ey, ex;
+
+/*
+ * 核心思路是遍历在k次转向内能够到达的地方
+ * 在此前提下，终点被遍历便意味着能到达
+ */
 int main() {
     int n;
     char ch;
@@ -26,22 +31,23 @@ int main() {
             for (int j = 1; j <= b; ++j) {
                 cin >> ch;
                 if (ch == '*') {
-                    s[i][j] = -2;
+                    s[i][j] = -2; // 禁区
                 } else {
-                    s[i][j] = -1;
+                    s[i][j] = -1; // 非禁区
                 }
             }
         }
         cin >> k >> by >> bx >> ey >> ex;
-        s[bx][by] = 0;
-        for (int l = 0; l <= k; ++l) {
+        s[bx][by] = 0; // 起始点
+
+        for (int l = 0; l <= k; ++l) { // 转向k次，起始点转向不算所以从0开始
             for (int i = 1; i <= a; ++i) {
                 for (int j = 1; j <= b; ++j) {
-                    if (s[i][j] == l) {
+                    if (s[i][j] == l) { // 确定可行动点
 
 
                         for (int m = j+1; m <= b; ++m) {
-                            if (s[i][m] == -2 || s[i][m] == l) break;
+                            if (s[i][m] == -2 || s[i][m] == l) break; // 遇到禁区或者已行动点，停止
                             s[i][m] = l + 1;
                         }
                         for (int m = j-1; m >= 1; --m) {
@@ -63,14 +69,7 @@ int main() {
                 }
             }
         }
-//        for (int i = 1; i <= a; ++i) {
-//            for (int j = 1; j <= b; ++j) {
-//                cout << s[i][j] << ' ';
-//            }
-//            cout << endl;
-//        }
-//        cout << s[ex][ey] << endl;
-        if (s[ex][ey] == -1) {
+        if (s[ex][ey] == -1) { // 检查是否遍历到
             cout << "no" << endl;
         } else {
             cout << "yes" << endl;
